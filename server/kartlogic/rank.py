5827f7bf82eb5b15894d3d0faa99c85aee9349a6
Joshua Groppe <joshua.groppe@peoplematter.com>
2017-10-29 19:45:49 -0400

    Implement unit tests in server-side code: add a few tests to rank function

--- server/kartlogic/rank.py
+++ server/kartlogic/rank.py
@@ -1,46 +1,88 @@
 #!/usr/bin/python2.7
-import data.players
-import data.games
+import data.players as player_data
+import data.games as game_data
+
+
+def calculate_average(games_played, total_score):
+    if games_played != 0:
+        average = total_score / games_played
+    else:
+        average = 0.0
+
 
 def average_individual():
+    # instantiate the ranking board
+    board = instantiate_empty_array()
 
-	# instantiate the ranking board
-	board = []
+    # for each player
+    for player_cursor in player_data.all_players():
 
-	# for each player
-	for player_cursor in data.players.all_players():
+        # initialize totals
+        games_played = 0
+        total_score = 0.0
 
-		# add up the player's game data
-		games_played = 0
-		total_score = 0.0
+        # get their last 25 games
+        for game_cursor in game_data.games_for_player(player_cursor['_id'], 25):
 
-		# get their last 25 games
-		for game_cursor in data.games.games_for_player(player_cursor['_id'], 25):
+            score_entry = get_player_score_entry(game_cursor)
 
-			# get their score from the game
-			score = game_cursor['scores'][0]
+            # add game data to totals
+            games_played += get_number_of_games_played(game_cursor)
+            total_score += get_player_score(score_entry)
 
-			# add it to their total
-			games_played += int(game_cursor.get('games', 0))
-			total_score += float(score.get('score', 0.0))
+        average = calculate_average(games_played, total_score)
 
-		# calculate the player's average
-		if games_played != 0:
-			average = total_score / games_played
-		else:
-			average = 0.0
+        # create the player's board entry
+        player = create_player_entry(player_cursor, average)
 
-		# create the player's board entry
-		player = {
-			'name': player_cursor['name'],
-			'character': player_cursor['character'],
-			'average': round(average, 2)
-		}
+        # add them to the board
+        board.append(player)
 
-		# add them to the board
-		board.append(player)
+    # sort by average
+    board = sorted(board, key=lambda player: player['average'], reverse=True)
 
-	# sort by average
-	board = sorted(board, key=lambda player: player['average'], reverse=True)
+    return board
 
-	return board
+
+def instantiate_empty_array():
+    return []
+
+
+def get_player_score_entry(game):
+    return game['scores'][0]
+
+
+def get_player_score(score_entry):
+    return float(score_entry.get('score', 0.0))
+
+
+def get_number_of_games_played(cursor):
+    return int(cursor.get('games', 0))
+
+
+def calculate_average(games_played, total_score):
+    if games_played != 0:
+        return total_score / games_played
+    else:
+        return 0.00
+
+
+def create_player_entry(cursor, average):
+    return {
+        'id': get_player_id(cursor),
+        'name': get_player_name(cursor),
+        'character': get_player_character(cursor),
+        'average': average
+    }
+
+
+def get_player_id(cursor):
+    return cursor['_id']
+
+
+def get_player_name(cursor):
+    return cursor.get('name', '<Unknown>')
+
+
+def get_player_character(cursor):
+    return cursor.get('character', '<Unknown>')
---
+++ server/test/rank_test.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python2.7
+import unittest
+import kartlogic.rank as rank
+
+
+class RankTests(unittest.TestCase):
+
+    def testInstantiateEmptyArray(self):
+
+        # Arrange
+        empty_array = []
+
+        # Act
+        result = rank.instantiate_empty_array()
+
+        # Assert
+        self.assertEqual(result, empty_array)
+
+    def testGetPlayerScoreEntry(self):
+
+        # Arrange
+        game = {
+            'scores': [4]
+        }
+
+        # Act
+        result = rank.get_player_score_entry(game)
+
+        # Assert
+        self.assertEqual(result, 4)
+
+    def testGetNumberOfGamesPlayed(self):
+
+        # Arrange
+        count = 5
+        cursor = {
+            'games': count
+        }
+
+        # Act
+        result = rank.get_number_of_games_played(cursor)
+
+        # Assert
+        self.assertEqual(result, count)
+
+    def testGetPlayerScore(self):
+
+        # Arrange
+        score = 25
+        score_entry = {
+            'score': score
+        }
+
+        # Act
+        result = rank.get_player_score(score_entry)
+
+        # Assert
+        self.assertEquals(result, score)
+
+    def testCalculateAverage_WhenGamesPlayed_ReturnsAverage(self):
+
+        # Arrange
+        games_played = 5
+        score = 75
+
+        # Act
+        result = rank.calculate_average(games_played, score)
+
+        # Assert
+        self.assertEqual(result, 15.00)
+
+    def testCalculateAverage_WhenNoGamesPlayed_ReturnsZero(self):
+
+        # Arrange
+        games_played = 0
+        score = 0
+
+        # Act
+        result = rank.calculate_average(games_played, score)
+
+        # Assert
+        self.assertEqual(result, 0.00)
+
+    def testGetPlayerId(self):
+
+        # Arrange
+        id = 'U33213'
+        cursor = {
+            '_id': id
+        }
+
+        # Act
+        result = rank.get_player_id(cursor)
+
+        # Assert
+        self.assertEqual(result, id)
+
+    def testGetPlayerName_WhenCursorContainsName_ReturnsName(self):
+
+        # Arrange
+        name = 'groppe'
+        cursor = {
+            'name': name
+        }
+
+        # Act
+        result = rank.get_player_name(cursor)
+
+        # Assert
+        self.assertEqual(result, name)
+
+    def testGetPlayerName_WhenCursorDoesNotContainName_ReturnsUnknown(self):
+
+        # Arrange
+        cursor = {}
+
+        # Act
+        result = rank.get_player_name(cursor)
+
+        # Assert
+        self.assertEqual(result, '<Unknown>')
+
+    def testGetPlayerCharacter_WhenCursorContainsCharacter_ReturnsCharacter(self):
+
+        # Arrange
+        character = 'link'
+        cursor = {
+            'character': character
+        }
+
+        # Act
+        result = rank.get_player_character(cursor)
+
+        # Assert
+        self.assertEqual(result, character)
+
+    def testGetPlayerCharacter_WhenCursorDoesNotContainCharacter_ReturnsUnknown(self):
+
+        # Arrange
+        cursor = {}
+
+        # Act
+        result = rank.get_player_character(cursor)
+
+        # Assert
+        self.assertEqual(result, '<Unknown>')
+
+    def testCreatePlayerEntry(self):
+
+        # Arrange
+        id = 'U39293'
+        character = 'link'
+        name = 'groppe'
+        cursor = {
+            '_id': id,
+            'name': name,
+            'character': character
+        }
+        average = 1.23
+
+        # Act
+        result = rank.create_player_entry(cursor, average)
+
+        # Assert
+        player = {
+            'id': id,
+            'name': name,
+            'character': character,
+            'average': average
+        }
+        self.assertDictEqual(result, player)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()

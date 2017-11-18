f6ed9f003387dfde7435773d2f202c7fa385375e
Joshua Groppe <joshua.groppe@peoplematter.com>
2017-11-18 17:41:39 -0500

    Add usage help command

--- server/slack.py
+++ server/slack.py
@@ -9,7 +9,7 @@ import util.slack as slackutil
 
 # compile regular expressions for slash command parameter strings
 pattern_help = re.compile('^help$')
-pattern_ranking = re.compile('^ranking$')
+pattern_ranking = re.compile('^rankings$')
 pattern_played = re.compile('^(played\s+\d+\s+games)((,\s+(<@\w+\|\w+>)\s+([0-9]+))+)$')
 pattern_characters = re.compile('^characters$')
 pattern_add_character = re.compile('^(add character\\s)(\".*\"\\s)([^\\s]+)$')
@@ -24,6 +24,7 @@ def handler(event, context):
         return webutil.respond_unauthorized("Invalid Slack token")
 
     command_text = input_data.get('text', 'help')
+
     if pattern_ranking.match(command_text):
         return handle_rankings()
     elif pattern_played.match(command_text):
@@ -41,16 +42,32 @@ def handler(event, context):
 
 
 def handle_help():
-    return webutil.respond_success('Help')
+    help_text = '*Usage:* /mario <command>\n*Commands:*\n```'
+    help_text += '\n1. rankings'
+    help_text += '\n2. played <#> games, <user1> <score1>, <user2> <score2>[, <user3> <score3>]'
+    help_text += '\n3. characters'
+    help_text += '\n4. add character "<character name>" <character image url>'
+    help_text += '\n5. my character is "<valid character name>"'
+    help_text += '\n6. my name is "<name you want to be called>"'
+    help_text += '```'
+    return webutil.respond_success(help_text)
 
 
 def handle_rankings():
+    # retrieve the rankings
     rankings = kartlogic.rank.average_individual()
+
+    # initialize the text table
     table = prettytable.PrettyTable(['Rank', 'Player', 'Character', 'Average'])
+
+    # add each ranking entry to the table
     for index, player in enumerate(rankings):
         table.add_row([(index + 1), player['name'], player['character'], player['average']])
+
+    # covert the table to format that Slack will understand
     table_string = '```' + table.get_string(border=True) + '```'
     slack_response = slackutil.in_channel_response(table_string)
+
     return webutil.respond_success_json(slack_response)
 
 

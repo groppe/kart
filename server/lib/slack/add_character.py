de28b973b0c93ab46474a1bdea15127937b02a75
Joshua Groppe <joshua.groppe@peoplematter.com>
2018-01-25 11:09:39 -0500

    correct ltrim() to lstrip() for add_character

--- server/lib/slack/add_character.py
+++ server/lib/slack/add_character.py
@@ -9,12 +9,12 @@ def handle(command_text):
     character_components = re.split(r'[\"\"]', command_text)
     character = {
         'name': character_components[1],
-        'image': character_components[2].ltrim()
+        'image': character_components[2].lstrip()
     }
     character_data.add_character(character)
     slack_body = slackutil.in_channel_response_as_user(
         'I\'m alive!',
         character_components[1],
-        character_components[2].ltrim()
+        character_components[2].lstrip()
     )
     return webutil.respond_success_json(slack_body)

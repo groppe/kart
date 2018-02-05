c30d9685239607883aeaee73618651f694f7d1b2
Joshua Groppe <joshua.groppe@peoplematter.com>
2018-02-04 19:01:01 -0500

    Update response for adding a player

--- server/lib/slack/add_player.py
+++ server/lib/slack/add_player.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 import re
 import lib.webutil as webutil
+import slack.util as slackutil
 from lib.data import players as player_data
 
 def handle(command_text):
@@ -9,7 +10,7 @@ def handle(command_text):
 
     player = player_data.get_player(new_player_id)
     if player is not None:
-        return webutil.respond_success('A player record for you already exists.')
+        return webutil.respond_success('A player record for <@' + new_player_id + '> already exists.')
 
     player_data.add_player(new_player_id)
-    return webutil.respond_success('Alright, <@' + new_player_id + '>, you\'re ready to play!')
+    return webutil.respond_success(slackutil.in_channel_response('Alright, <@' + new_player_id + '> is ready to play!'))

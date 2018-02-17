976e121cd616e0a7f3b09b4ffc26d5cb8d858695
Joshua Groppe <scclemson@gmail.com>
2018-02-17 10:36:31 -0500

    update Slack help command with add player syntax

--- src/lib/slack/help.py
+++ src/lib/slack/help.py
@@ -8,7 +8,8 @@ def handle():
     help_text += '\n2. played <#> games, <user1> <score1>, <user2> <score2>[, <user3> <score3>]'
     help_text += '\n3. characters'
     help_text += '\n4. add character "<character name>" <character image url>'
-    help_text += '\n5. my character is "<valid character name>"'
-    help_text += '\n6. my name is "<name you want to be called>"'
+    help_text += '\n5. add player @<player>'
+    help_text += '\n6. my character is "<valid character name>"'
+    help_text += '\n7. my name is "<name you want to be called>"'
     help_text += '```'
     return webutil.respond_success(help_text)

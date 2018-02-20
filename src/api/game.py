06d5399af0b2303ff30c00849ae2d955057e66a4
Joshua Groppe <scclemson@gmail.com>
2018-02-20 09:56:24 -0500

    serialize characters using bson

--- src/api/game.py
+++ src/api/game.py
@@ -8,6 +8,15 @@ from bson.json_util import dumps
 def create(event, context):
     logging.critical(event)
 
+    request_body = webutil.parse_event_for_request_body(event)
+    if not request_body:
+        return webutil.respond_bad_request('to add a game, include the number of games and at least two players')
+
+    request_data = json.loads(request_body)
+    slack_id = request_data.get('slack_id', None)
+    if not slack_id:
+        return webutil.respond_bad_request('to create a player, include at least their Slack id')
+
 def all(event, context):
     queryStringParameters = event.get('queryStringParameters') or {}
     size = queryStringParameters.get('size', 25)
@@ -34,7 +43,7 @@ def get(event, context):
     if game is None:
         return webutil.respond_not_found('a game with that id does not exist')
     
-    return webutil.respond_success_json(game)
+    return webutil.respond_success_json(dumps(game))
 
 def update(event, context):
     logging.critical(event)

8a3bcc0e921d1fa0a4501e99547dac601fa1af81
Joshua Groppe <scclemson@gmail.com>
2018-02-22 16:41:05 -0500

    first go at integration tests with newman

--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 npm install -g serverless
 npm install -g serverless-python-requirements
+npm install -g newman
 cd src
 pip install -r requirements.txt
 sls plugin install -n serverless-python-requirements
---
+++ build/integration_test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+newman run tests/Player_API_Test.postman_collection.json -e tests/Kart-Dev.postman_environment.json

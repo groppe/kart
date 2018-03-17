a7f664d148792f30ee8a3cd74d0f37d453a7b291
Joshua Groppe <scclemson@gmail.com>
2018-03-17 15:32:50 -0400

    Add website to game results

--- src/lib/slack/played_game.py
+++ src/lib/slack/played_game.py
@@ -46,6 +46,7 @@ def handle(command_text):
         player_cursor = player_data.get_player(score['player_id']) or {}
         response_text += '\n' + player_cursor.get('name', '<Unknown>')
         response_text += ': ' + str(score['average'])
+    response_text += '\nCheck out the updated rankings at http://kartskills.com/dashboard!'
 
     slack_body = slackutil.in_channel_response(response_text)
     return webutil.respond_success_json(json.dumps(slack_body))
